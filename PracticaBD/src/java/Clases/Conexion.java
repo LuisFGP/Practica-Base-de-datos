b015594929a1d60f38c40708861df169176a78b2
luisfgarciapolvo5IM9 <42345565+luisfgarciapolvo5IM9@users.noreply.github.com>
2018-09-27 22:03:12 -0500

    Practica base de datos con login y registro
    @emmanuel789

---
+++ PracticaBD/src/java/Clases/Conexion.java
@@ -0,0 +1,94 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Clases;
+import java.sql.*;
+
+
+/**
+ *
+ * @author Alumno
+ */
+public class Conexion {
+    String url;
+    String User;
+    String Pass;
+    Connection con;
+    Statement sta;
+    ResultSet res;
+    
+    public Conexion(){
+        url="jdbc:mysql://localhost/Usuarios";
+        User="root";
+        Pass="n0m3l0";
+    }
+    
+    public String Con(String nombre, String contra){
+        String ok="No";
+        
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            con=DriverManager.getConnection(url, User, Pass);
+            sta=con.createStatement();
+            
+            res=sta.executeQuery("select * from Usuario where usuario='"+nombre+"' and contraseña='"+contra+"';");
+            if(res.next()){
+                ok="Oka";
+            }
+            else{
+                ok="Falso";
+            }
+        }
+        
+        catch(Exception e){
+            ok=e.getMessage();
+            System.out.println(e.getMessage());
+        }
+         
+        return ok;
+    }
+    
+    
+    public String Rol(String Email, String Contra){
+        String Rol="No hay rol";
+        
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            con=DriverManager.getConnection(url, User, Pass);
+            sta=con.createStatement();
+            
+            res=sta.executeQuery("select * from Usuario where usuario='"+Email+"' and contraseña='"+Contra+"';");
+            if(res.next()){
+                Rol=res.getString("idrol");
+            }
+        }
+        
+        catch(Exception e){
+            System.out.println(e.getMessage());
+        }
+         
+        return Rol;
+    }
+    
+    public String Insert(String Usu,String Contra, String Cor, String Nom, String Ap, String Am, String tel, int idrol){
+        String ok="error";
+        
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            con=DriverManager.getConnection(url, User, Pass);
+            sta=con.createStatement();
+            
+            res=sta.executeQuery("insert into Usuario values('10','"+Usu+"','"+Contra+"','"+Cor+"','"+Nom+"','"+Ap+"','"+Am+"','"+tel+"','"+idrol+"');");
+            ok="Usuario Dado de alta";
+        }
+        
+        catch(Exception e){
+            ok=e.getMessage();
+            System.out.println(e.getMessage());
+        }
+        return ok;
+    }
+}
+
---
+++ PracticaBD/src/java/Clases/Rol.java
@@ -0,0 +1,68 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Clases;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author Alumno
+ */
+
+public class Rol implements Serializable {
+
+    private Long idrol;
+    private String rol;
+
+    public Rol() {
+    }
+
+    public Rol(Long idrol) {
+        this.idrol = idrol;
+    }
+
+    public Long getIdrol() {
+        return idrol;
+    }
+
+    public void setIdrol(Long idrol) {
+        this.idrol = idrol;
+    }
+
+    public String getRol() {
+        return rol;
+    }
+
+    public void setRol(String rol) {
+        this.rol = rol;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (idrol != null ? idrol.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Rol)) {
+            return false;
+        }
+        Rol other = (Rol) object;
+        if ((this.idrol == null && other.idrol != null) || (this.idrol != null && !this.idrol.equals(other.idrol))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Clases.Rol_1[ idrol=" + idrol + " ]";
+    }
+    
+}
